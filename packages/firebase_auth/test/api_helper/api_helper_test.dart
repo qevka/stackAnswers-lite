cdfa3d61b18377967b21e37638902c5b9519ceb1
Caleb Maccarone <>
2022-02-12 22:59:34 -0500

    getting close to tests working

--- packages/firebase_auth/test/api_helper/api_helper_test.dart
+++ packages/firebase_auth/test/api_helper/api_helper_test.dart
@@ -9,7 +9,12 @@ import 'package:mockito/mockito.dart';
 
 import 'api_helper_test.mocks.dart';
 
-@GenerateMocks([http.Client])
+/// What I want to test here
+/// when a user sends a request to the server I want to mock the response so I can test it reliably
+/// Send Email password -> return a success response and test that it outputs the correct error types
+/// Send signup password and email -> make sure responses are handled.
+
+@GenerateMocks([], customMocks: [MockSpec<http.Client>(as: #MockClient, returnNullOnMissingStub: true)])
 void main() {
   group("ApiHelper Test", () {
     test("post", () async {
@@ -18,20 +23,19 @@ void main() {
       final helper = ApiHelper(client: client);
       final authData = File('packages/firebase_auth/test/test_data/auth_response_login.json');
       String authJson = await authData.readAsString();
-      final passwordResetData = File('packages/firebase_auth/test/test_data/auth_response_password_reset.json');
-      String passJson = await authData.readAsString();
-      const login = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=1234';
-      const resetPass = 'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=1234';
-      const refreshToken = 'https://securetoken.googleapis.com/v1/token?key=1234';
-      // login
-      when(client.post(Uri.parse(login))).thenAnswer((_) async => http.Response(authJson, 200));
-      expect(helper.post(url: refreshToken), authJson);
-      when(client.post(Uri.parse(resetPass))).thenAnswer((_) async => http.Response(passJson, 404));
-      expect(helper.post(url: refreshToken), isA<AuthErrorResponse>());
-      // // reset Password
-      // when(client.post(Uri.parse(resetPass))).thenAnswer((_) async => http.Response(passJson, 200));
-      // // refresh Token
-      // when(client.post(Uri.parse(refreshToken))).thenAnswer((_) async => http.Response(authJson, 200));
+      final authErrorData = File('packages/firebase_auth/test/test_data/auth_error_response.json');
+      String authError = await authErrorData.readAsString();
+
+      const login = 'login';
+      const error = 'error';
+
+      // when(client.post(Uri.parse(login), headers: null, body: null, encoding: null))
+      //     .thenAnswer((_) async => http.Response(authJson, 200));
+      when(client.post(Uri.parse(error))).thenAnswer((_) async => http.Response(authError, 400));
+      var data = await helper.post(url: error);
+
+      expect(data.body, isA<AuthErrorResponse>());
+      reset(client);
     });
   });
 }
